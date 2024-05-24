2bc0f475e687ea82dd3a8844e1d94a1c1461cccd
KrolLew23 <aniabandola@gmail.com>
2024-05-24 21:57:07 +0200

    posters

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,9 @@
 import { getTrending } from '../API/Api';
 import { useEffect, useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
+import styles from './Movies.module.css';
 
-const useTrending = () => {
+const Home = () => {
   const [trendingMovies, setTrendingMovies] = useState([]);
   const location = useLocation();
 
@@ -13,17 +14,26 @@ const useTrending = () => {
   return (
     <>
       <h1>Trending today</h1>
-      <ul>
+      <div className={styles['movies-list']}>
         {trendingMovies.map(movie => (
-          <li key={movie.id}>
-            <NavLink to={`/movies/${movie.id}`} state={{ from: location }}>
-              {movie.title}
-            </NavLink>
-          </li>
+          <NavLink key={movie.id} to={`/movies/${movie.id}`} state={{ from: location }}>
+            <div className={styles['movie-item']}>
+              <img
+                src={
+                  movie.poster_path
+                    ? `https://www.themoviedb.org/t/p/w500/${movie.poster_path}`
+                    : `https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg`
+                }
+                alt={movie.title}
+                className={styles['movie-poster']}
+              />
+              <span className={styles['movie-title']}>{movie.title}</span>
+            </div>
+          </NavLink>
         ))}
-      </ul>
+      </div>
     </>
   );
 };
 
-export default useTrending;
+export default Home;
